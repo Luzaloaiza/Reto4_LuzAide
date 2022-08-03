caad0ece4a0e9fbb88b3e377fc39878c9d3d6ded
Luzaloaiza <110011119+Luzaloaiza@users.noreply.github.com>
2022-08-03 09:54:40 -0500

    Add files via upload

---
+++ Controlador/EnumTipoDocumento.java
@@ -0,0 +1,13 @@
+// * @author Luz Aide Loaiza Garcia G55
+package Controlador;
+
+
+public enum EnumTipoDocumento {
+    Seleccionaunaopcion,
+    CedulaCuidadania,
+    CedulaExtranjera,
+    LibretaMilitar,
+    Pasaporte,
+    otro                    
+}
+
